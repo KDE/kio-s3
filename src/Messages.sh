d34a3604ca736d22258add1d7b254ef4afc4db5c
Carl Schwan <carlschwan@kde.org>
2026-03-06 12:08:09 +0100

    i18n: Enable i18n in the repo

--- src/Messages.sh
+++ src/Messages.sh
@@ -1,3 +1,11 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
-$XGETTEXT `find . -name \*.cpp -o -name \*.h` -o $podir/kio5_s3.pot
+# Invoke the extractrc script on all .ui, .rc, and .kcfg files in the sources.
+# The results are stored in a pseudo .cpp file to be picked up by xgettext.
+lst=`find . -name \*.rc -o -name \*.ui -o -name \*.kcfg`
+if [ -n "$lst" ] ; then
+    $EXTRACTRC $lst >> rc.cpp
+fi
+
+# Run xgettext to extract strings from all source files.
+$XGETTEXT `find . -name \*.cpp -o -name \*.h -o -name \*.qml` -o $podir/kio-s3.pot
